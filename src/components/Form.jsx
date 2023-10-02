64a70205611394ef6027f257e84aaf0443054680
Flor <florenciar504@gmail.com>
2023-10-01 22:24:46 -0300

    last details

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,12 +3,45 @@ import Input from "./Input";
 import { Icon } from "./Icon";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { useState } from "react";
+
 const Form = () => {
   const [originalInputs, setOriginalInputs] = useState(true);
   const [newInputs, setNewInputs] = useState(false);
+  const [nameInput, setNameInput] = useState("");
+  const [lastNameInput, setLastNameInput] = useState("");
+  const [emailInput, setEmailInput] = useState("");
+  const [numberInput, setNumberInput] = useState("");
+  const [areaInput, setAreaInput] = useState("");
+
   const addInputs = () => {
     setOriginalInputs(false);
     setNewInputs(true);
+  };
+  const handleSubmitForm = (event) => {
+    event.preventDefault()
+    const info = {
+      nombre: nameInput,
+      apellido: lastNameInput,
+      email: emailInput,
+      numero: numberInput,
+      area: areaInput,
+    };
+    console.log(info);
+  };
+  const handleInputName = (event) => {
+    setNameInput(event.target.value);
+  };
+  const handleInputLastName = (event) => {
+    setLastNameInput(event.target.value);
+  };
+  const handleInputEmailName = (event) => {
+    setEmailInput(event.target.value);
+  };
+  const handleInputNumberName = (event) => {
+    setNumberInput(event.target.value);
+  };
+  const handleInputAreaName = (event) => {
+    setAreaInput(event.target.value);
   };
   return (
     <div className="form_body">
@@ -20,19 +53,44 @@ const Form = () => {
         </p>
       </div>
       <div className="form-container">
-        <form className="inputs" >
+        <form className="inputs" onSubmit={handleSubmitForm}>
           {originalInputs && (
             <>
-              <Input placeholder="Nombre" type="text"></Input>
-              <Input placeholder="Apellido" type="text"></Input>
-              <Input placeholder="Correo electrónico" type="text"></Input>
+              <Input
+                placeholder="Nombre"
+                type="text"
+                value={nameInput}
+                onChange={handleInputName}
+              ></Input>
+              <Input
+                placeholder="Apellido"
+                type="text"
+                value={lastNameInput}
+                onChange={handleInputLastName}
+              ></Input>
+              <Input
+                placeholder="Correo electrónico"
+                type="text"
+                value={emailInput}
+                onChange={handleInputEmailName}
+              ></Input>
             </>
           )}
 
           {newInputs && (
             <>
-              <Input placeholder="+54 11 1234 1234" type="text"></Input>
-              <Input placeholder="Área de interés" type="text"></Input>
+              <Input
+                placeholder="+54 11 1234 1234"
+                type="number"
+                value={numberInput}
+                onChange={handleInputNumberName}
+              ></Input>
+              <Input
+                placeholder="Área de interés"
+                type="text"
+                value={areaInput}
+                onChange={handleInputAreaName}
+              ></Input>
             </>
           )}
           <div className="checkbox">
@@ -45,11 +103,19 @@ const Form = () => {
               sobre nuestros diplomados
             </label>
           </div>
+          {newInputs && (
+            <button className="button" type="submit">
+              Quiero hablar con un asesor
+            </button>
+          )}
         </form>
         <div className="autorization">
-          <button className="button" onClick={addInputs}>
-            Continuar
-          </button>
+          {originalInputs && (
+            <button className="button" onClick={addInputs}>
+              Continuar
+            </button>
+          )}
+
           <p className="autorizationTypography">
             Al hacer click arriba, nos autoriza a contactarlo/a mediante un
             sistema automatizado de llamadas al telefono indicado arriba con el
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,10 @@
 import "../styles/Input.css";
-const Input = ({ placeholder, type }) => {
+const Input = ({ placeholder, type, value, onChange}) => {
+  const inputClass = `text ${type === "number" ? "inputImage" : ""}`;
   return (
     <div className="inputText">
       <div className="input">
-        <input placeholder={placeholder} type={type} className="text"></input>
+        <input placeholder={placeholder} type={type} className={inputClass} value={value} onChange={onChange}></input>
       </div>
     </div>
   );
